c124e1f0da36b3ff3d7f4489fff1ff469a2b540e
Yann Dirson <yann.dirson@vates.fr>
2022-10-18 13:01:02 +0200

    Add support for globally disabling repo_gpgcheck
    While developing and/or installing from an already-verified ISO, yum's
    repo_gpgcheck just gets in the way.  This provides a general kill-switch
    for checking repomd.xml.asc, through commandline or answerfile.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.fr>

--- backend.py
+++ backend.py
@@ -322,6 +322,10 @@ def determineRepositories(answers, answers_pristine, main_repositories, update_r
         for i in answers_pristine['sources']:
             repos = repository.repositoriesFromDefinition(i['media'], i['address'])
             add_repos(main_repositories, update_repositories, repos)
+            repo_gpgcheck = answers.get('repo-gpgcheck', True)
+            for repo in repos:
+                if repo in main_repositories:
+                    repo.setRepoGpgCheck(repo_gpgcheck)
 
     # A single source coming from an interactive install
     if 'source-media' in answers_pristine and 'source-address' in answers_pristine:
