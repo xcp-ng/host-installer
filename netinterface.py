c27fe4294e7c94b8a4951fc424ff74461bd5cbe7
BenjiReis <benjamin.reis@vates.fr>
2023-05-30 10:36:16 +0200

    Add `NetInterfaceV6` to init an IPv6 interface
    Inherits from `NetInterface` to mutualize the code
    
    Signed-off-by: BenjiReis <benjamin.reis@vates.fr>

--- netinterface.py
+++ netinterface.py
@@ -16,7 +16,7 @@ def getTextOrNone(nodelist):
             rc = rc + node.data
     return rc == "" and None or rc.strip().encode()
 
-class NetInterface:
+class NetInterface(Object):
     """ Represents the configuration of a network interface. """
 
     Static = 1
@@ -348,3 +348,22 @@ class NetInterface:
 
         nic.addIPv6(modev6, ipv6addr, gatewayv6)
         return nic
+
+class NetInterfaceV6(NetInterface):
+    def __init__(self, mode, hwaddr, ipaddr=None, netmask=None, gateway=None, dns=None, domain=None, vlan=None):
+        super(NetInterfaceV6, self).__init__(None, hwaddr, None, None, None, None, None, vlan)
+
+        ipv6addr = None
+        if mode == self.Static:
+            assert ipaddr
+            assert netmask
+
+            ipv6addr = ipaddr + "/" + netmask
+            if dns == '':
+                dns = None
+            elif isinstance(dns, str):
+                dns = [ dns ]
+            self.dns = dns
+            self.domain = domain
+
+        self.addIPv6(mode, ipv6addr=ipv6addr, ipv6gw=gateway)
