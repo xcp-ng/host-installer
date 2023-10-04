6614456111584c113fa7ec78ac31edf2d4950ecf
Yann Dirson <yann.dirson@vates.fr>
2023-10-04 15:41:56 +0200

    findXenSourceBackups: log any exception caught
    - it is bad practice to "catch any"
    - not logging anything just hides information, which can be especially
      important here as the reason for a try/catch is not obvious (exceptions
      thrown by XenServerBackup.__init__?)
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.fr>

--- product.py
+++ product.py
@@ -540,7 +540,9 @@ def findXenSourceBackups():
                 if backup.version >= XENSERVER_MIN_VERSION and \
                         backup.version <= THIS_PLATFORM_VERSION:
                     backups.append(backup)
-        except:
+        except Exception as ex:
+            logger.log("findXenSourceBackups caught exception for partition %s" % (p,))
+            logger.logException(ex)
             pass
         if b:
             b.unmount()
