9ccab16fc582a6316cd05c4a28aa8b8bcff960aa
Thierry Escande <thierry.escande@vates.tech>
2024-08-02 16:54:02 +0200

    Ignore backup if boot modes don't match
    This patch checks the boot mode of an existing backups and disallows its
    restoration if it differs from the installer one. This uses the new
    helper function is_rootf_uefi().
    
    Signed-off-by: Thierry Escande <thierry.escande@vates.tech>

--- product.py
+++ product.py
@@ -622,6 +622,13 @@ def findXenSourceBackups():
             backup = XenServerBackup(p, b.mount_point)
             logger.log("Found a backup: %s" % (repr(backup),))
 
+            # Don't restore a BIOS backup with a UEFI installer and conversely
+            backup_is_uefi = is_rootfs_uefi(b.mount_point)
+            if backup_is_uefi != constants.UEFI_INSTALLER:
+                logger.log("findXenSourceBackups: ignoring, installer mode (%s) does not match backup boot mode (%s)" %
+                           ("uefi" if constants.UEFI_INSTALLER else "legacy BIOS", "uefi" if backup_is_uefi else "legacy BIOS" ))
+                raise StopIteration()
+
             if backup.version < XENSERVER_MIN_VERSION:
                 logger.log("findXenSourceBackups: ignoring, platform too old: %s < %s" %
                            (backup.version, XENSERVER_MIN_VERSION))
