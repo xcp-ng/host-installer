fdc25d25ff388fa78c6304c6e6c00b86325ad548
Yann Dirson <yann.dirson@vates.tech>
2025-03-28 16:03:37 +0100

    linstor: extract use of LINSTOR in ExistingInstallation
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- product.py
+++ product.py
@@ -28,6 +28,14 @@ THIS_PLATFORM_VERSION = Version.from_string(version.PLATFORM_VERSION)
 XENSERVER_7_0_0 = Version([2, 1, 0]) # Platform version
 XENSERVER_MIN_VERSION = XENSERVER_7_0_0
 
+def getLinstorVersion(rootfs_mount):
+    """ Returns the package version of the LINSTOR packages if any """
+    command = ['rpm', '--root', rootfs_mount, '-q', '--qf', '%{evr}', 'linstor-satellite']
+    rc, out = util.runCmd2(command, with_stdout=True)
+    if rc != 0:
+        return None
+    return out
+
 class ExistingInstallation:
     def __init__(self, primary_disk, boot_device, state_device):
         self.primary_disk = primary_disk
@@ -355,6 +363,9 @@ class ExistingInstallation:
                 pc.close()
             except:
                 pass
+
+            results['linstor-version'] = getLinstorVersion(self.state_fs.mount_point) or None
+            logger.info("LINSTOR version detected: %s" % (results['linstor-version'],))
 
         finally:
             self.unmount_state()
