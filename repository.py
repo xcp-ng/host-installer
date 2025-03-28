804e7420df0853e77ac29ec39bc4ac0ce30589e2
Yann Dirson <yann.dirson@vates.tech>
2025-03-28 16:11:54 +0100

    repository: reduce the number of hardcoded '/root/yum.conf' occurrences
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>

--- repository.py
+++ repository.py
@@ -187,7 +187,8 @@ class YumRepository(Repository):
         assert self._targets is not None
         url = self._accessor.url()
         logger.log("URL: " + str(url))
-        with open('/root/yum.conf', 'w') as yum_conf:
+        yum_conf_path = '/root/yum.conf'
+        with open(yum_conf_path, 'w') as yum_conf:
             yum_conf.write(self._yum_conf)
             yum_conf.write("""
 [install]
@@ -205,7 +206,7 @@ baseurl=%s
                 yum_conf.write(repo_config)
 
         self.disableInitrdCreation(mounts['root'])
-        installFromYum(self._targets, mounts, progress_callback, self._cachedir)
+        installFromYum(yum_conf_path, self._targets, mounts, progress_callback, self._cachedir)
         self.enableInitrdCreation()
 
     def installPackages(self, progress_callback, mounts):
@@ -427,7 +428,8 @@ history_record=false
     def isRepo(cls, accessor):
         if UpdateYumRepository.isRepo(accessor):
             url = accessor.url()
-            with open('/root/yum.conf', 'w') as yum_conf:
+            yum_conf_path = '/root/yum.conf'
+            with open(yum_conf_path, 'w') as yum_conf:
                 yum_conf.write(cls._yum_conf)
                 yum_conf.write("""
 [driverrepo]
@@ -442,7 +444,7 @@ baseurl=%s
                     yum_conf.write("password=%s\n" % (url.getPassword(),))
 
             # Check that the drivers group exists in the repo.
-            rv, out = util.runCmd2(['yum', '-c', '/root/yum.conf',
+            rv, out = util.runCmd2(['yum', '-c', yum_conf_path,
                                     'group', 'summary', 'drivers'], with_stdout=True)
             if rv == 0 and 'Groups: 1\n' in out.strip():
                 return True
@@ -793,11 +795,11 @@ def findRepositoriesOnMedia(drivers=False):
 
     return repos
 
-def installFromYum(targets, mounts, progress_callback, cachedir):
+def installFromYum(yum_conf_path, targets, mounts, progress_callback, cachedir):
         # Use a temporary file to avoid deadlocking
         stderr = tempfile.TemporaryFile()
 
-        yum_command = ['yum', '-c', '/root/yum.conf',
+        yum_command = ['yum', '-c', yum_conf_path,
                        '--installroot', mounts['root'],
                        'install', '-y'] + targets
         logger.log("Running yum: %s" % ' '.join(yum_command))
@@ -873,12 +875,14 @@ def installFromRepos(progress_callback, repos, mounts, kernel_alt, linstor_versi
 
     logger.log("installFromRepos, kernel_alt=%s" % (kernel_alt,))
     cachedir = "var/cache/yum/installer"
+    yum_conf_path = '/root/yum.conf'
+
     for repo in repos:
         repo._accessor.start()
 
     try:
         # Build a yum config
-        with open('/root/yum.conf', 'w') as yum_conf:
+        with open(yum_conf_path, 'w') as yum_conf:
             yum_conf.write(_generateYumConf(cachedir))
             for repo in repos:
                 url = repo._accessor.url()
@@ -912,7 +916,7 @@ baseurl=%s
                             'linstor-controller-%s' % linstor_version,
                             ])
 
-        installFromYum(targets, mounts, progress_callback, cachedir)
+        installFromYum(yum_conf_path, targets, mounts, progress_callback, cachedir)
         repos[0].enableInitrdCreation()
     finally:
         for repo in repos:
